8fce52fbb3786883e78e105d88d4ea29b0170752
mirhasankhan <mirhasan000034@gmail.com>
2025-08-15 20:24:35 +0600

    expense api craetd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import { useCurrentUser } from "@/redux/features/auth/authSlice";
+import { useAppSelector } from "@/redux/hooks";
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
-  return <div>sdfsdfsdf</div>;
+  const { email } = useAppSelector(useCurrentUser);
+
+  return (
+    <div>
+      {email ? (
+        "jhee"
+      ) : (
+        <div className="flex items-center gap-8 justify-center">
+          <Link href="/auth/login">           
+            <button className="bg-primary text-white px-6 py-2 rounded-[8px] font-medium">Login</button>
+          </Link>
+          <Link href="/auth/register">           
+            <button className="border border-primary rounded-[8px] text-primary bg-transparent px-6 py-2 font-medium">Sign Up</button>
+          </Link>
+         
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HomePage;
